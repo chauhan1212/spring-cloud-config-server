3afa85db392ad1a3b7d593eb9e55491899b6f964
Vijay Chauhan <chauhanvijay81@gmail.com>
2020-12-28 17:55:13 +0530

    completed project

--- src/main/java/com/example/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ src/main/java/com/example/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -8,6 +8,14 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class SpringCloudConfigServerApplication {
 
+	/*
+	 * http://localhost:8888/application/default
+	 * 
+	 * http://localhost:8888/spring-boot-config/default
+	 * 
+	 * Github Link:
+	 * https://github.com/chauhan1212/cloud-config.git
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
 	}
